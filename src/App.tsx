d3da0d2601a59c770254ff873673003145d1d17a
Andrey <55235312+andrew133@users.noreply.github.com>
2022-04-08 21:24:37 +0300

    08

--- src/App.tsx
+++ src/App.tsx
@@ -3,25 +3,9 @@ import './App.css';
 import {Todolist} from "./Todolist";
 
 export function App() {
-    let tasks1 = [
-        { id: 1, title: "HTML&CSS", isDone: true },
-        { id: 2, title: "JS", isDone: true },
-        { id: 3, title: "ReactJS", isDone: false }
-    ]
-    let tasks2 = [
-        { id: 1, title: "Hello world", isDone: true },
-        { id: 2, title: "I am Happy", isDone: false },
-        { id: 3, title: "Yo", isDone: false }
-    ]
-    return (
-        <div className="App">
-            <Todolist siga={'What to learn1111'} tasks={tasks1} />
-            <Todolist pomidor={'What to learn2222' } tasks={tasks2}/>
-
-
-        </div>
-
-    );
+   return(
+<div>Hello word</div>
+   );
 }
 
 
