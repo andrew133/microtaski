6f909cc8023548ed35d1464a4843bd1e96efdd17
Andrey <55235312+andrew133@users.noreply.github.com>
2022-04-12 21:02:03 +0300

    12

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,18 @@ import React from 'react';
 import './App.css';
 import {Header} from "./site/Header";
 import {Body} from "./site/Body";
+import {Futer} from "./site/Futer";
 
 export function App() {
-   return(
-       <>
-          <Header/>
-          <Body/>
-       </>
+    return (
+        <>
+            <Header/>
+            <Body/>
+            <Futer/>
+        </>
 
-   );
+    );
 }
-
 
 
 export default App;
